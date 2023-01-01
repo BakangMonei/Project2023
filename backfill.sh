e8c54c6d7d4d3d8f3b29ce873bffcd3893696820
BakangMonei <bakangmonei2@gmail.com>
2023-01-01 12:00:00 +0200

    Commit on 2023-01-01

---
+++ backfill.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Start date (modify if you want to start on a different day)
+start_date="2023-01-01"
+
+# Loop through each day of the year
+for i in {0..364}
+do
+    # Calculate the date for each iteration
+    current_date=$(date -d "$start_date + $i days" +%Y-%m-%d)
+
+    # Create a placeholder file or modify an existing file
+    echo "Commit for $current_date" > commit.txt
+
+    # Add the file to the staging area
+    git add .
+
+    # Make a commit with the current date as the commit date
+    GIT_COMMITTER_DATE="$current_date 12:00:00" git commit --date="$current_date 12:00:00" -m "Commit on $current_date"
+done
