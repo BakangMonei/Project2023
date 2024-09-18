98761260ac19badc3635f7858ec3c458ae477d1e
BakangMonei <bakangmonei2@gmail.com>
2024-09-18 12:00:00 +0200

    Commit on 2024-09-18

--- backfill.sh
+++ backfill.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Start date (modify if you want to start on a different day)
-start_date="2024-09-11"
+start_date="2024-09-18"
 
 # Loop through each day of the year
 for i in {0..364}
